4d6a8294b0bf1bee9d1c9e542dc1f6569a586b2a
Somrat-Hossen <33490220+Somrat-Hossen@users.noreply.github.com>
2018-01-03 16:44:22 +0600

    Create 791A - Bear and Big Brother.cpp

---
+++ 791A - Bear and Big Brother.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#include<iomanip>
+#define PI acos(-1)
+int arr[10000];
+
+using namespace std;
+
+int check(int n)
+{
+
+}
+
+int main()
+{
+    int n,i,sum=0,m,tmp,cnt=0,x,y,z,j;
+    cin>>x>>y;
+
+    while(1)
+    {
+    x*=3;
+    y*=2;
+    cnt++;
+
+    if(x>y)
+    {
+        break;
+    }
+    }
+    cout<<cnt<<endl;
+
+    return 0;
+}
+
