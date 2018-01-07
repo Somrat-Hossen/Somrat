7849d889f51c5b549b6c34574f32edd952b7c57a
Somrat-Hossen <33490220+Somrat-Hossen@users.noreply.github.com>
2018-01-08 00:18:12 +0600

    Create 141A Amusing Joke.cpp

---
+++ 141A Amusing Joke.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+#include<iomanip>
+#define PI acos(-1)
+using namespace std;
+int arr[1000];
+int dp[1000];
+vector<int>vec;
+stack<int>stk;
+
+int check(int n)
+{
+
+}
+
+int main()
+{
+    string st,str,s,sum;
+    cin>>st>>str>>s;
+
+    sum=st+str;
+    sort(sum.begin(),sum.end());
+    sort(s.begin(),s.end());
+
+  if(sum==s)
+     {
+      cout<<"YES"<<endl;
+     }
+  else
+     {
+      cout<<"NO"<<endl;
+     }
+
+    return 0;
+}
+
+
+#include <bits/stdc++.h>
+#include<iomanip>
+#define PI acos(-1)
+using namespace std;
+int arr[1000];
+int dp[1000];
+vector<int>vec;
+stack<int>stk;
+
+int check(int n)
+{
+
+}
+
+int main()
+{
+    string st,str,s,sum;
+    cin>>st>>str>>s;
+
+    sum=st+str;
+    sort(sum.begin(),sum.end());
+    sort(s.begin(),s.end());
+
+  if(sum==s)
+     {
+      cout<<"YES"<<endl;
+     }
+  else
+     {
+      cout<<"NO"<<endl;
+     }
+
+    return 0;
+}
+
+
